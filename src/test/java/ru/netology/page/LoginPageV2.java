487b64cab699c2f55e9ec2e04cd951137dae5deb
OlgaNikulina <nikulinaov@inbox.ru>
2020-05-15 01:05:39 +0500

    methods with transactions added

--- src/test/java/ru/netology/page/LoginPageV2.java
+++ src/test/java/ru/netology/page/LoginPageV2.java
@@ -10,33 +10,53 @@ import static com.codeborne.selenide.Selenide.$$;
 
 
 public class LoginPageV2 {
-  private SelenideElement loginField = $("[data-test-id=login] input");
-  private SelenideElement passwordField = $("[data-test-id=password] input");
-  private SelenideElement loginButton = $("[data-test-id=action-login]");
-  private SelenideElement buttonContinue = $("span.button__text");
-  private SelenideElement buttonReplenish = $("span.button__text");
-  private SelenideElement transferPage = $("div.App_appContainer__3jRx1");
-  private SelenideElement amountField = $("[data-test-id='amount'] input");
-  private SelenideElement fromField = $("[data-test-id='from'] input");
-  private SelenideElement replenishFinalButton = $$("button").find(exactText("пополнить"));
-  private SelenideElement cartsInfoPage = $("div.App_appContainer__3jRx1");
+    private SelenideElement loginField = $("[data-test-id=login] input");
+    private SelenideElement passwordField = $("[data-test-id=password] input");
+    private SelenideElement loginButton = $("[data-test-id=action-login]");
+    private SelenideElement buttonContinue = $("span.button__text");
+    private SelenideElement balance01CartBefore = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']");
+    private SelenideElement balance02CartBefore = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']");
+    private SelenideElement buttonReplenish1 = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] .button__text");
+    private SelenideElement buttonReplenish2 = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] .button__text");
+    private SelenideElement transferPage = $("div.App_appContainer__3jRx1");
+    private SelenideElement amountField = $("[data-test-id='amount'] input");
+    private SelenideElement fromField = $("[data-test-id='from'] input");
+    private SelenideElement replenishFinalButton = $$("button").find(exactText("пополнить"));
+    private SelenideElement balance01CartAfter = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']");
+    private SelenideElement balance02CartAfter = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']");
+    private SelenideElement cartsInfoPage = $("App_appContainer__3jRx1");
 
 
-  public VerificationPage validLogin(DataHelper.AuthInfo info) {
-    loginField.setValue(info.getLogin());
-    passwordField.setValue(info.getPassword());
-    loginButton.click();
-    buttonContinue.click();
-    return new VerificationPage();
-  }
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        buttonContinue.click();
+        return new VerificationPage();
+    }
 
-  public DataHelper transferMoneyBetweenCart(DataHelper.CartInfo dataHelper) {
-    buttonReplenish.click();
-    transferPage.shouldBe(visible);
-    amountField.setValue(String.valueOf(dataHelper.getAmount()));
-    fromField.setValue(dataHelper.getCartNumber1());
-    replenishFinalButton.click();
-    cartsInfoPage.shouldBe(visible);
-    return (DataHelper) cartsInfoPage;
-  }
+    public DataHelper transferMoneyFrom01Cart(DataHelper.CartInfo dataHelper) {
+        balance01CartBefore.shouldBe(visible);
+        balance02CartBefore.shouldBe(visible);
+        buttonReplenish2.click();
+        transferPage.shouldBe(visible);
+        amountField.setValue(String.valueOf(dataHelper.getAmount()));
+        fromField.setValue(dataHelper.getCartNumber1());
+        replenishFinalButton.click();
+        balance01CartAfter.shouldBe(visible);
+        balance02CartAfter.shouldBe(visible);
+        return (DataHelper) cartsInfoPage;
+    }
+
+    public DataHelper transferMoneyFrom02Cart(DataHelper.CartInfo dataHelper) {
+        balance01CartBefore.shouldBe(visible);
+        balance02CartBefore.shouldBe(visible);
+        buttonReplenish1.click();
+        transferPage.shouldBe(visible);
+        amountField.setValue(String.valueOf(dataHelper.getAmount()));
+        fromField.setValue(dataHelper.getCartNumber2());
+        replenishFinalButton.click();
+        cartsInfoPage.shouldBe(visible);
+        return (DataHelper) cartsInfoPage;
+    }
 }
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -3,7 +3,6 @@ package ru.netology.web.test;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import ru.netology.page.LoginPageV2;
-import ru.netology.page.LoginPageV3;
 import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -11,32 +10,25 @@ import static com.codeborne.selenide.Selenide.open;
 class MoneyTransferTest {
     @Test
     void shouldTransfer100From01Cart() {
-      open("http://localhost:9999");
-      val loginPage = new LoginPageV2();
-      val authInfo = DataHelper.getAuthInfo();
-      val verificationPage = loginPage.validLogin(authInfo);
-      val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-      verificationPage.validVerify(verificationCode);
-      DataHelper.CartInfo dataHelper = DataHelper.transfer100From01Cart();
-      val transferMoneyBetweenCarts = loginPage.transferMoneyBetweenCart(dataHelper);
+        open("http://localhost:9999");
+        val loginPage = new LoginPageV2();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+        DataHelper.CartInfo dataHelper = DataHelper.transfer100From01Cart();
+        loginPage.transferMoneyFrom01Cart(dataHelper);
     }
 
-  @Test
-  void shouldTransferMoneyBetweenOwnCardsV2() {
-    open("http://localhost:9999");
-    val loginPage = new LoginPageV2();
-    val authInfo = DataHelper.getAuthInfo();
-    val verificationPage = loginPage.validLogin(authInfo);
-    val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-  }
-
-  @Test
-  void shouldTransferMoneyBetweenOwnCardsV3() {
-    val loginPage = open("http://localhost:9999", LoginPageV3.class);
-    val authInfo = DataHelper.getAuthInfo();
-    val verificationPage = loginPage.validLogin(authInfo);
-    val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-  }
+    @Test
+    void shouldTransfer100From02Cart() {
+        open("http://localhost:9999");
+        val loginPage = new LoginPageV2();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+        DataHelper.CartInfo dataHelper = DataHelper.transfer100From02Cart();
+        loginPage.transferMoneyFrom02Cart(dataHelper);
+    }
 }
